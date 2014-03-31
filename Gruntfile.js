44d3e196040f08191308e083982916f70ffbef16
Ourai Lin <ourairyu@hotmail.com>
2014-04-01 00:23:08 +0800

    U: config of Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,10 @@ module.exports = function( grunt ) {
   var npmTasks = [
       "grunt-contrib-concat",
       "grunt-contrib-jade",
-      "grunt-contrib-sass"
+      "grunt-contrib-compass",
+      "grunt-contrib-copy",
+      "grunt-contrib-cssmin",
+      "grunt-contrib-clean"
     ];
   var index = 0;
   var length = npmTasks.length;
@@ -17,6 +20,9 @@ module.exports = function( grunt ) {
     meta: {
       src: "src",
       sass: "src/scss",
+      helpers: "<%= meta.sass %>/helpers",
+      base: "<%= meta.sass %>/base",
+      dest: "dest",
       build: "build",
       tests: "<%= meta.build %>/tests",
       tasks: "<%= meta.build %>/tasks",
@@ -24,20 +30,48 @@ module.exports = function( grunt ) {
       cmpts: "<%= meta.tests %>/components"
     },
     concat: {
+      helpers: {
+        src: ["<%= meta.helpers %>/_css3.scss",
+              "<%= meta.helpers %>/_variables.scss",
+              "<%= meta.helpers %>/_functions.scss",
+              "<%= meta.helpers %>/_mixins.scss"],
+        dest: "<%= meta.dest %>/_helpers.scss"
+      },
       vendors: {
         src: ["vendors/normalize.css/normalize.css"],
-        dest: "<%= meta.sass %>/_vendors.scss"
+        dest: "<%= meta.dest %>/_vendors.scss"
+      },
+      rules: {
+        src: ["<%= meta.base %>/_reset.scss",
+              "<%= meta.base %>/_components.scss",
+              "<%= meta.base %>/_g11n.scss"],
+        dest: "<%= meta.dest %>/_rules.scss"
+      },
+      core: {
+        src: ["<%= meta.sass %>/rules.scss"],
+        dest: "<%= meta.dest %>/<%= pkg.name %>.scss"
       }
     },
-    sass: {
+    compass: {
       compile: {
-        files: [{
-          expand: true,
-          cwd: "<%= meta.src %>",
-          src: ["*.scss"],
-          dest: "<%= meta.src %>",
-          ext: ".css"
-        }]
+        options: {
+          sassDir: "<%= meta.dest %>",
+          cssDir: "<%= meta.dest %>"
+        }
+      }
+    },
+    copy: {
+      sass: {
+        expand: true,
+        cwd: "<%= meta.sass %>",
+        src: ["layouts/*"],
+        dest: "dest",
+        filter: "isFile"
+      }
+    },
+    clean: {
+      compiled: {
+        src: ["dest/*.scss", "!dest/_helpers.scss"]
       }
     },
     /*jade: {
@@ -52,22 +86,14 @@ module.exports = function( grunt ) {
           "<%= meta.dest %>/layout.html": "<%= meta.src %>/views/layouts/layout.jade"
         }
       }
-    }
-    compass: {
-      compile: {
-        options: {
-          sassDir: "<%= meta.src %>",
-          cssDir: "<%= meta.src %>"
-        }
-      }
-    },
+    }*/
     cssmin: {
       minify: {
         files: {
           "<%= meta.dest %>/<%= pkg.name %>.min.css": "<%= meta.dest %>/<%= pkg.name %>.css"
         }
       }
-    },*/
+    },
     watch: {
       css: {
         files: ["<%= meta.dest %>/**/*.scss"],
@@ -84,5 +110,5 @@ module.exports = function( grunt ) {
     grunt.loadNpmTasks(npmTasks[index]);
   }
 
-  grunt.registerTask("default", ["sass"]);
+  grunt.registerTask("default", ["concat", "copy", "compass", "cssmin", "clean"]);
 };
